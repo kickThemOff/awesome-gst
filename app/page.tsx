62cb29fa6722d5ee4bd589caf7d88d8b9a28c390
Tingyu Huang_Timmy <755407216@qq.com>
2025-03-22 22:46:50 +1300

    feat: integrate header and contract components with wallet connection functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import ConnectWallet from "../components/connectWallet"
+import ConnectWallet from "../components/connectWallet";
+import AccountsSection from "../components/AccountsSection"
+import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+    <main className="flex items-center justify-center min-h-screen bg-gray-900">
       <ConnectWallet />
+      <AccountsSection />
+      <Header/>
+      
     </main>
   );
 }
