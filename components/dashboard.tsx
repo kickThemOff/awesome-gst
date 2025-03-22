61c8967c8c3dc2c2d30f4cd22d150a8ba4c5e7d0
itha462 <itha462@aucklanduni.ac.nz>
2025-03-23 02:38:04 +1300

    Got the Pay button working, transaction able to send a contract request and response if payment is successful or failed

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { parseEther } from "ethers";
+import { Contract } from "ethers";
+import { BrowserProvider } from "ethers";
+
 function Invoice() {
 
     const invoices = [
@@ -11,6 +17,197 @@ function Invoice() {
         { id: 2, name: "Stripe", GST: "+GST80", amount: "+80" },
         { id: 3, name: "Stripe", GST: "+GST80", amount: "+80" },
     ];
+
+    const fixedAddress = "0x660594b90a8ea7F1D0b43bEAB4Fe3c734dc20A7A"; // Replace with the actual Ethereum address
+    const fixedAmount = 1781310000; // Adjust amount as needed
+
+
+    // Function to handle the transaction
+    async function handlePayment() {
+        if (!window.ethereum) {
+            alert("Please install MetaMask to make transactions!");
+            return;
+        }
+
+        try {
+            const provider = new BrowserProvider(window.ethereum);
+            await provider.send("eth_requestAccounts", []); // Request user to connect wallet
+            const signer = await provider.getSigner();
+
+            // Replace with your contract address & ABI
+            const contractAddress = "0x90b6fa1BC66dBF3a6E2Fa49C0b525081B55Bd399"; // Replace with actual contract address
+            const contractABI = [ // Replace with actual ABI
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "IRDAccount",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint256",
+                            "name": "amount",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "GSTWithdrawn",
+                    "type": "event"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "oldaddr",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "newaddr",
+                            "type": "address"
+                        }
+                    ],
+                    "name": "IRDAccountChangedr",
+                    "type": "event"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "payee",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint256",
+                            "name": "amount",
+                            "type": "uint256"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint256",
+                            "name": "code",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "PayeeTransfer",
+                    "type": "event"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "newIRDAccount",
+                            "type": "address"
+                        }
+                    ],
+                    "name": "changeIRDAccount",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "hasPaymentBeenReceived",
+                    "outputs": [
+                        {
+                            "internalType": "bool",
+                            "name": "",
+                            "type": "bool"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "splitGST",
+                    "outputs": [
+                        {
+                            "internalType": "bool",
+                            "name": "",
+                            "type": "bool"
+                        }
+                    ],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "taxInfo",
+                    "outputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "gstBalance",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "mainBalance",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "defiBalance",
+                            "type": "uint256"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "payee",
+                            "type": "address"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "amount",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "code",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "transferToPayee",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "withdrawGST",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                }
+            ];
+
+
+            
+            const contract = new Contract(contractAddress, contractABI, signer);
+
+            // Execute the contract function
+            const tx = await contract.transferToPayee(fixedAddress, fixedAmount, 0);
+            await tx.wait(); // Wait for transaction confirmation
+
+            alert("Payment successful!");
+        } catch (error) {
+            console.error("Transaction failed:", error);
+            alert("Transaction failed! Check the console for details.");
+        }
+    }
 
     return (
       <>
@@ -26,7 +223,7 @@ function Invoice() {
         </div>
 
         {invoices.map((invoice) => (
-            <div key={invoices.id} className="child-container">
+            <div key={invoice.id} className="child-container">
                 <p>
                     <span className="amount">{invoice.amount}</span> 
                     <span className="currency">NZDD</span>
@@ -36,9 +233,9 @@ function Invoice() {
                 </div>
                 <div className="bottom-container">
                     <span className="color">From:</span>
-                    <span className="color">Smith.co</span>
+                    <span className="color">{invoice.from}</span>
                     <button className="reject-button">Reject</button>
-                    <button className="reject-button">Pay</button>
+                    <button className="reject-button" onClick={handlePayment}>Pay</button>
                 </div>
             </div>
             ))}
